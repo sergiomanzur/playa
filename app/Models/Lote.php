5142dda1d7524d82aaccdebbda954eb8bc9599d8
sergiomanzur <sergiom200@hotmail.com>
2023-02-17 14:32:40 -0600

    Added promesas, pagos and lotes tables and models

---
+++ app/Models/Lote.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use MongoDB\Driver\Manager;
+
+class Lote extends Model
+{
+    use HasFactory;
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function manzana(): BelongsTo
+    {
+        return $this->belongsTo(Manzana::class);
+    }
+}
