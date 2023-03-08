e1473acd4374742362ef3a09eca07d7fe519cc62
sergiomanzur <sergiom200@hotmail.com>
2023-03-08 13:19:13 -0600

    Update dashboard and pie chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -26,6 +26,7 @@ class DashboardController extends Controller
             $balance = $lote->balances->total;
             $promesa = $lote->promesas->cantidad;
             $credito = $lote->balances->credito;
+            $pagos = $lote->pagos;
 
             $sum_pagos = 0;
 
@@ -36,6 +37,8 @@ class DashboardController extends Controller
             $balance_de_pagos_realizados = $sum_pagos;
             $balance_pendiente_por_pagar = $balance - $promesa - $balance_de_pagos_realizados;
             $balance_a_credito = $credito;
+
+            $payment_per_month = $credito / $lote->balances->plan_de_pagos;
 
             //(Amount paid / Total worth) x 100%
             $amount_paid = $promesa + $balance_de_pagos_realizados;
@@ -54,7 +57,10 @@ class DashboardController extends Controller
                 'balance_pendiente_por_pagar' => $balance_pendiente_por_pagar,
                 'balance_a_credito' => $balance_a_credito,
                 'balance_pagado' => $amount_paid,
-                'porcentaje_por_pagar' => $porcentaje_por_pagar
+                'porcentaje_por_pagar' => $porcentaje_por_pagar,
+                'rows' => $lote->balances->plan_de_pagos,
+                'pago_por_mes' => $payment_per_month,
+                'pagos' => $pagos
             ]
         ]);
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,14 +9,41 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div style="display: flex;">
-                        <div style="width: 70%;">
+                    <div class="flex flex-col md:flex-row">
+                        <div  class="md:w-3/4 p-4">
                             <h2 style="font-size: x-large">Bienvenido a tu cuenta de Playa Hermosa</h2>
-                            <h3>{{$data['user']['name']}}</h3>
+                            <p>{{$data['user']['name']}}</p>
                             <p>{{$data['manzana']['nombre']}} - {{$data['lote']['nombre']}}</p>
-                            <div id="piechart" style="margin-top: 20px; width: 90%; height: 300px;"></div>
+{{--                            <div id="piechart" style="margin-top: 20px; width: 90%; height: 300px;"></div>--}}
+                            <div style="width:100%; margin: auto; text-align: center; margin-top: 15px;">
+                                <h2 style="margin-bottom: 15px;font-size: x-large; font-weight: bolder">
+                                    PORCENTAJE POR PAGAR: {{$data['porcentaje_por_pagar']}}%</h2>
+                                <div class="chart-container">
+                                    <h2 class="chart-label"></h2>
+                                    <canvas id="car-chart" style="max-width:100%;max-height: 300px;">
+                                    </canvas>
+                                </div>
+                                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                            </div>
+                            <div style="margin:auto; margin-top: 20px; padding-right: 25%; padding-left: 25%">
+                                <table>
+                                    <tr>
+                                        <td>Pago Promesa</td>
+                                        <td>${{number_format($data['promesa'],2)}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Pago a Meses</td>
+                                        <td>${{number_format($data['credito'],2)}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Balance Total</td>
+                                        <td><strong>${{number_format($data['balance'],2)}}</strong></td>
+                                    </tr>
+                                </table>
+                            </div>
+
                         </div>
-                        <div style="width: 30%; padding: 15px; margin-top: 30px;">
+                        <div class="md:w-1/4">
 
                             <h3>Balance a crédito</h3>
                             <p>${{number_format($data['balance_a_credito'],2)}}</p>
@@ -34,31 +61,73 @@
                         </div>
                     </div>
 
-                    <!-- INICIA  SCRIPT DE PIE CHART -->
-                    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-                    <script type="text/javascript">
-                        google.charts.load('current', {'packages':['corechart']});
-                        google.charts.setOnLoadCallback(drawChart);
+                    <div class="main-table">
+                        <div class="table-wrapper">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th class="header">Pago no.</th>
+                                    <th class="header">Monto de pago</th>
+                                    <th class="header">Pagos realizados</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @for ($i = 1; $i <= $data['rows']; $i++)
+                                    <tr>
+                                        <td>{{ $i }}</td>
+                                        <td>${{number_format($data['pago_por_mes'],2)}}</td>
+                                        <td>${{(isset($data['pagos'][$i-1]->cantidad)) ? number_format($data['pagos'][$i-1]->cantidad,2) : '0.00'}}</td>
+                                    </tr>
+                                @endfor
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
 
-                        function drawChart() {
 
-                            var data = google.visualization.arrayToDataTable([
-                                ['Concepto', 'Cantidad'],
-                                ['Pagado',     {{$data['balance_pagado']}}],
-                                ['Por Pagar', {{$data['balance_pendiente_por_pagar']}}],
-                            ]);
+                    <script>
+                        // Get data for chart
+                        const totalAmount = {{$data['balance']}};
+                        const amountPaid = {{$data['balance_pagado']}};
+                        const amountLeft = totalAmount - amountPaid;
+                        const percentLeft = (amountLeft / totalAmount) * 100;
+                        const percentPaid = 100 - percentLeft;
+                        const centerText = `${percentPaid.toFixed(0)}%`;
 
-                            var options = {
-                                title: 'Porcentajes',
-                                backgroundColor: 'white',
-                                legend: {position: 'top', textStyle: {color: 'blue', fontSize: 16}},
-                                titleTextStyle: {color: 'blue'}
-                            };
+                        // Create chart
+                        const ctx = document.getElementById('car-chart').getContext('2d');
+                        const carChart = new Chart(ctx, {
+                            type: 'doughnut',
+                            data: {
+                                labels: ['Cantidad Pagada', 'Cantidad Restante'],
+                                datasets: [{
+                                    label: 'Pagos',
+                                    data: [percentPaid, percentLeft],
+                                    backgroundColor: [
+                                        'rgba(54, 162, 235, 0.6)',
+                                        'rgba(255, 99, 132, 0.6)'
+                                    ],
+                                    borderColor: [
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 99, 132, 1)'
+                                    ],
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                responsive: true,
+                                legend: {
+                                    display: true,
+                                    labels: {
+                                        fontColor: 'black'
+                                    }
+                                }
+                            }
+                        });
 
-                            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+                        const chartLabel = document.querySelector('.chart-label');
+                        chartLabel.textContent = centerText;
 
-                            chart.draw(data, options);
-                        }
                     </script>
                 </div>
             </div>
