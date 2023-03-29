c9df016ec4becbfb73f6d61c6e5d3b28a50dd2d6
sergiomanzur <sergiom200@hotmail.com>
2023-03-28 19:55:31 -0600

    test fixing sql requirements for DO

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 
@@ -14,7 +15,8 @@ return new class extends Migration
      */
     public function up()
     {
-        \Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
+        DB::statement('SET SESSION sql_require_primary_key=0');
+
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
