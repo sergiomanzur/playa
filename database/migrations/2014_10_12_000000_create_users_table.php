8facd908888a7759da48432a6420f5d1684182bf
sergiomanzur <sergiom200@hotmail.com>
2023-03-28 19:49:53 -0600

    test fix for schema create

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -13,6 +14,7 @@ return new class extends Migration
      */
     public function up()
     {
+        \Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
